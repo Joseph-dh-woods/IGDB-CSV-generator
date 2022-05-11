1ed5bd0ebcfcddf6f020643702464014edaf45b9
Joseph-dh-woods <74608884+Joseph-dh-woods@users.noreply.github.com>
2022-05-11 19:28:39 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+from igdb.wrapper import IGDBWrapper
+import json
+import time
+from google.colab import auth
+import gspread
+from google.auth import default
+import pandas as pd
+
+def makeItem(jason):
+  game = jason.split('\n')
+  return game
+
+auth.authenticate_user()
+creds, _ = default()
+
+gc = gspread.authorize(creds)
+
+worksheet = gc.open('ID_INPUT').sheet1
+IDlist = []
+# get_all_values gives a list of rows.
+rows = worksheet.get_all_values()
+for x in rows:
+  IDlist.append(x[0])
+  
+  getToken = requests.post("https://id.twitch.tv/oauth2/token?client_id=p4drjdnuirwnzc9i1rs74fv8sdhtbi&client_secret=51ez2706qa9f957aqypha41cdzgxxs&grant_type=client_credentials")
+s = getToken.text
+sI = s.split("\"")
+accessToken = sI[3]
+accessID = "p4drjdnuirwnzc9i1rs74fv8sdhtbi"
+wrapper = IGDBWrapper(accessID, accessToken)
+games = []
+for ID in IDlist:
+  rq = 'fields name, age_ratings, category, cover, first_release_date, genres, involved_companies, summary, themes, url; where id = ' + ID + ';'
+  byteArray = wrapper.api_request(
+              'games',
+              rq
+            )
+  myJson = byteArray.decode('utf8').replace("'", '"')
+  games.append(makeItem(myJson))
+  time.sleep(.1)
+
+display(games)
+# pd.DataFrame.from_records(games)
